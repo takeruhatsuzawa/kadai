7f02b211c05108201ba04e361e1bed0840d06b23
初沢岳 <takeruhatsuzawa1028@gmail.com>
2015-12-26 17:08:30 +0900

    3回目コミット

---
+++ 8/index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+<ul>
+<li><a href="get.php">form(get)</a></li>
+<li><a href="post.php">form(post)</a></li>
+<li><a href="hensu.php">変数</a></li>
+<li><a href="hairetsu.php">配列</a></li>
+<li><a href="seigyo.php">制御構文</a></li>
+<li><a href="kansu.php">関数</a></li>
+</ul>
+<ul>
+<li><a href="write.php">ファイル書き込み</a></li>
+<li><a href="read.php">ファイル読み込む</a></li>
+</ul>
+</body>
+</html>
