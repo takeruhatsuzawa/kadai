0c90c300ccca2c8ae3a50d9afcc39d022ef997d3
初沢岳 <takeruhatsuzawa1028@gmail.com>
2016-01-07 23:36:44 +0900

    コミット

--- 6/input_finish.php
+++ 6/input_finish.php
@@ -5,5 +5,17 @@
 </head>
 <body>
     <div><a href="input_enq.php">ありがとうございました</div>
+        
+<?php
+
+$file = fopen(“data/data.txt”,“a”);
+flock($file, LOCK_EX); 
+fputs($file, “This is a Pen.” . PHP_EOL); 
+flock($file, LOCK_UN); 
+fclose($file); 
+
+?>
+        
+
 </body>
 </html>
